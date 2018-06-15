0ecd196b3affb6c58c449dc32244828ad354f622
Viktoriia Chernyshuk <vickierose.ch@gmail.com>
2018-06-15 18:59:04 +0300

    added cart item to menu

--- src/elements/Navigation.js
+++ src/elements/Navigation.js
@@ -18,6 +18,10 @@ export default class Navigation extends Component {
         <Menu.Item as={Nav} to="/shop" name='Shop' ><Icon name="shopping bag"/><Responsive minWidth={768}>Shop</Responsive></Menu.Item>
         <Menu.Item as={Nav} to="/delivery" name='Delivery and Payments' ><Icon name="bicycle"/><Responsive minWidth={768}>Delivery and Payments</Responsive></Menu.Item>
         <Menu.Item as={Nav} to="/contacts" name='Contacts' ><Icon name="address card"/><Responsive minWidth={768}>Contacts</Responsive></Menu.Item>
+
+        <Menu.Menu position='right'>
+        <Menu.Item><Icon name="shopping cart"/></Menu.Item>
+        </Menu.Menu>
       </Menu>
       </div>
     )
